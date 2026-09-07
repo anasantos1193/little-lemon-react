e37813d9a71b661d6d41fc6a9ec8ff0bb667edd3
anasantos1193 <anasantos1193@gmail.com>
2026-09-07 21:33:53 +0100

    Add responsive layout and CSS styling

---
+++ src/Footer.js
@@ -0,0 +1,23 @@
+function Footer() {
+  return (
+    <footer>
+      <p>© 2026 Little Lemon</p>
+
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/menu">Menu</a>
+          </li>
+        </ul>
+      </nav>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/Main.js
@@ -0,0 +1,10 @@
+function Main() {
+  return (
+    <main>
+      <h1>Little Lemon</h1>
+      <p>Welcome to Little Lemon restaurant.</p>
+    </main>
+  );
+}
+
+export default Main;
